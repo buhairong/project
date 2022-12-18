ae16d5a84fbc511ccf6313c653bb12fd2c7f8478
buhairong <22858111@qq.com>
2022-12-18 19:18:36 +0800

    省市区联动

--- vue3/m-components/src/components/chooseArea/index.vue
+++ vue3/m-components/src/components/chooseArea/index.vue
@@ -1,26 +1,72 @@
 <template>
     <div>
-        <el-select placeholder="请选择省份" v-model="province">
-            <el-option></el-option>
+        <el-select placeholder="请选择省份" v-model="province" clearable>
+          <el-option 
+            v-for="item in AllAreas"
+            :key="item.code"
+            :value="item.code"
+            :label="item.name"
+          >
+          </el-option>
         </el-select>
 
-        <el-select placeholder="请选择城市" v-model="city">
-            <el-option></el-option>
+        <el-select placeholder="请选择城市" v-model="city" :disabled="!province" clearable>
+          <el-option 
+            v-for="item in selectCity"
+            :key="item.code"
+            :value="item.code"
+            :label="item.name"
+            >
+          </el-option>
         </el-select>
 
-        <el-select placeholder="请选择区域" v-model="area">
-            <el-option></el-option>
+        <el-select placeholder="请选择区域" v-model="area" :disabled="!province || !city" clearable>
+          <el-option 
+            v-for="item in selectArea"
+            :key="item.code"
+            :value="item.code"
+            :label="item.name"
+            >
+          </el-option>
         </el-select>
     </div>
 </template>
 
 <script lang="ts" setup>
-import { ref } from 'vue'
+import { ref, watch } from 'vue'
 import AllAreas from './lib/pca-code.json'
 
 const province = ref<string>('')
 const city = ref<string>('')
 const area = ref<string>('')
+const selectCity = ref<any[]>([])
+const selectArea = ref<any[]>([])
+
+watch(() => province.value, val => {
+  city.value = ''
+  area.value = ''
+  if (val) {
+    const findItem = AllAreas.find(item => item.code === province.value)
+    if (findItem) {
+      selectCity.value = findItem.children
+    } else {
+      selectCity.value = []
+    }
+  }
+})
+
+watch(() => city.value, val => {
+  area.value = ''
+  if (val) {
+    const findItem = selectCity.value.find(item => item.code === city.value)
+    if (findItem) {
+      selectArea.value = findItem.children
+    } else {
+      selectArea.value = []
+    }
+  }
+})
+
 </script>
 
 <style lang="scss" scoped>
